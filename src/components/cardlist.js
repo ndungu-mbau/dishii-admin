413558dd5f9b32d6a0efcda7825e2b95a303c0b7
Nelson Mbau <mbau.ndungu@gmail.com>
2020-09-26 02:22:13 +0300

    feat: Add card list component

---
+++ src/components/cardlist.js
@@ -0,0 +1,45 @@
+import React from "react";
+import usePagination from "../utils/paginate"
+
+const CardList = props => {
+  const { current, display, pages, next, previous, set } = usePagination({ items: props.data, size: props.pagination })
+  return (
+    <div className={`card border-light shadow-md mb-4 ${props.styles?.card}`}>
+      <div className={`card-header ${props.styles?.header}`}>
+        <h3 className={props.styles?.title}>{props.title}</h3>
+      </div>
+      <div className={`card-body ${props.styles?.body}`}>
+          <div className="row">
+              <div className={`col-md-${12/props.row_count}`}>
+                {display.map(row => {
+                  return (props.children(row))
+                })}
+              </div>
+          </div>
+      </div>
+      <div className={`cardFooter ${props.styles?.footer}`}>
+        <nav>
+          <ul class={`pagination mb-0 ${props.styles?.pagination}`}>
+            <li class={`page-item ${current === 1 ? "disabled" : ''}`}>
+              <a className="page-link" onClick={previous}>
+                <i class="fas fa-arrow-left"></i>
+              </a>
+            </li>
+            {Array(pages).fill(0).map((num, i) => i+1).map(page => (
+              <li class={`page-item ${current === page ? "active" : ''}`}>
+                <a className="page-link" onClick={() => set(page)}>{page}</a>
+              </li>
+            ))}
+            <li class={`page-item ${current === Math.ceil(props.data/props.pagination) ? "disabled" : ''}`}>
+              <a className="page-link" onClick={next}>
+                <i class="fas fa-arrow-right"></i>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
+};
+
+export default CardList
