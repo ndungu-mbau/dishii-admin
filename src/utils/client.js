cb0fb6f495800717c80cccc8bda450e7ce09b3aa
Nelson Mbau <mbau.ndungu@gmail.com>
2020-10-04 00:51:15 +0300

    feat: USe @apollo/client

--- src/utils/client.js
+++ src/utils/client.js
@@ -1,4 +1,4 @@
-import { ApolloClient } from "apollo-client"
+import { ApolloClient } from "@apollo/client"
 import { createHttpLink } from 'apollo-link-http'
 import { setContext } from 'apollo-link-context'
 import { InMemoryCache } from 'apollo-cache-inmemory'
