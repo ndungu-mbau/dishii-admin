721a05bcf5074f5e66184815c55e3d83e1afdfa0
Nelson Mbau <mbau.ndungu@gmail.com>
2020-10-07 04:24:50 +0300

    feat: Restructure routing

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ import {
 } from 'react-router-dom'
 
 import apolloClient from "./utils/client"
+import { Store } from "./utils/state"
 
+import index from "./index-page"
 import admin from "./admin"
 import client from "./client"
 import cashier from "./cashier"
@@ -17,13 +19,16 @@ import cashier from "./cashier"
 function App() {
   return (
     <ApolloProvider client={apolloClient}>
-      <Router>
-        <Switch>
-          <Route path="/admin" component={admin} />
-          <Route path="/cashier" component={cashier} />
-          <Route path="/" component={client} />
-        </Switch>
-      </Router>
+      <Store>
+        <Router>
+          <Switch>
+            <Route path="/" exact component={index} />
+            <Route path="/admin" component={admin} />
+            <Route path="/cashier" component={cashier} />
+            <Route path="/client" component={client} />
+          </Switch>
+        </Router>
+      </Store>
     </ApolloProvider>
   );
 }
--- src/client/index.js
+++ src/client/index.js
@@ -5,14 +5,16 @@ import { Switch, Route } from 'react-router-dom'
 import menus from './menus'
 import meals from './meals'
 import meal from './meal'
+import tables from "./tables"
 
 export default props => {
   return (
     <main className='content'>
       <Switch>
-        <Route path='/' exact component={menus} />
-        <Route path='/:id' exact component={meals} />
-        <Route path='/:id/:meal' exact component={meal} />
+        <Route path='/client/' exact component={tables} />
+        <Route path='/client/menus' exact component={menus} />
+        <Route path='/client/menus/:id' exact component={meals} />
+        <Route path='/client/menus/:id/:meal' exact component={meal} />
       </Switch>
     </main>
   )
